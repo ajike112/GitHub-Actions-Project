bb8bd9a52d474cb30fe36c376ba8637b00ced687
yourUsername <yourEmail@example.com>
2026-09-09 03:21:51 -0500

    Fixing the secret for the Docker build step

--- git-command.sh
+++ git-command.sh
@@ -5,6 +5,6 @@ echo "Add files and do local commit"
 git add .
 echo "checking status"
 git status
-git commit -am "Updating github-actions-role trust relationships and permissions"
+git commit -am "Fixing the secret for the Docker build step"
 echo "Pushing to Github Repository"
 git push -u origin master
